02144c55a8a0da94cc038412933de8e849d42fee
loong.woo <longwunet@outlook.com>
2023-11-11 13:56:37 +0000

    feat: 修改 ViteSystemTS 方法

--- src/vite-plugin-system-ts.ts
+++ src/vite-plugin-system-ts.ts
@@ -1,23 +1,39 @@
 import type { Plugin } from 'vite'
 
-export const ViteSystemTS = (): Plugin => ({
-    name: 'vite-system-ts',
-    apply: 'serve',
-    handleHotUpdate({server, file}) {
-        server.ws.send({
-            type: 'custom',
-            event: 'vps:hot-file-update',
-            data: file
-        })
-        return []
-    },
-    transform(code, id) {
-        return {
-            code: `if (import.meta.hot) {
-                import.meta.hot.on('vps:hot-file-update', (data: string) => {
-                  console.log(data)
-                })
-              }`
-        }
-    }
-})
+export interface ViteSystemTSOption {
+	/**
+	 * 客户端处理热更新的入口文件，默认是 main.ts/main.js。
+	 * 如果是微前端（Single-SPA），请使用主应用的入口文件。
+	 */
+	hotEnter?: string
+}
+
+export const ViteSystemTS = (option: ViteSystemTSOption): Plugin => {
+	const _hotEnter = option.hotEnter ?? 'main'
+
+	return {
+		name: 'vite-system-ts',
+		apply: 'serve',
+		handleHotUpdate({server, file}) {
+			server.ws.send({
+				type: 'custom',
+				event: 'vps:hot-file-update',
+				data: file
+			})
+			return []
+		},
+		transform(code, id) {
+			if (_hotEnter === 'main' && !/main.(j|t)s$/.test(_hotEnter) || !id.includes(_hotEnter)) return code
+
+			return {
+				code: `${code}
+				
+				if (import.meta.hot) {
+					import.meta.hot.on('vps:hot-file-update', (data: string) => {
+						console.log(data)
+					})
+				}`
+			}
+		}
+	}
+}
